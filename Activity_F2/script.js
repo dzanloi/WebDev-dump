1e0e740c04c6d34da5fc34a89f1b04db9667d4e6
dzanloi <carreon.johnloi@gmail.com>
2023-12-06 00:22:30 +0800

    git add .

---
+++ Activity_F2/script.js
@@ -0,0 +1,11 @@
+function shownum(value) {
+    let total_input = document.getElementById(`total`);
+    let sub1_input = document.getElementById(`sub1`);
+    let sub2_input = document.getElementById(`sub2`);
+    let sub3_input = document.getElementById(`sub3`);
+    sub3_input.value = value;
+
+    let total = 0;
+    total = sub1_input.value + sub2_input.value + sub3_input.value;
+    total_input.value = total;
+}

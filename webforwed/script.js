3046d6a202c3f3b109f5a62c8fe959c85f3ce51c
dzanloi <carreon.johnloi@gmail.com>
2024-03-03 23:10:38 +0800

    almost done with footer

--- webforwed/script.js
+++ webforwed/script.js
@@ -1,10 +1,13 @@
-document.querySelector('.navigations ul li:first-child a').addEventListener('click', function(event) {
+document.querySelector('.navigations ul li:first-child a').addEventListener('click', function() {
     // event.preventDefault();
     window.location.href = this.getAttribute('href');
   });
 
-  document.getElementById("logo").addEventListener("click", function() {
+  document.getElementById("logo").addEventListener(`click`, function() {
     window.location.href = "index.html";
   });
 
-  document.getElementById()
+  document.getElementById().addEventListener(`click`, function() {
+    window.location.href = ``;
+  });
+
